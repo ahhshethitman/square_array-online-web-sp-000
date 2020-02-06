04dca2d14df03bc36ff10419b5dedb33ed43b6b0
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-06 17:27:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
 
 
 def square_array(array)
-  new_array = []
-  array.each do |i| 
-    new_array << i ** 2 
-    return new_array
-  end
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
